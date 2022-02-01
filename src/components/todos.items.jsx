f358385c6f898a06b45696721540cbe87e2e715e
Gopala <guptaraja727@gmail.com>
2022-02-02 01:31:33 +0530

    cc

--- src/components/todos.items.jsx
+++ src/components/todos.items.jsx
@@ -1,7 +1,10 @@
-export const Todositem = ({title})=> {
+export const Todositem = ({title, status,id,DeleteTodo})=> {
     return (
         <div>
-            <h1>{title}</h1>
+            <h1>{title}-{status? "Done":"Not done"}</h1>
+            <button onClick={()=>{
+                DeleteTodo(id)
+            }}>Delete Item</button>
         </div>
     )
 }
--- src/components/todos.jsx
+++ src/components/todos.jsx
@@ -1,18 +1,27 @@
 import { useState } from "react"
 import { Todosinput } from "./todos.input"
 import { Todositem } from "./todos.items"
-
+import { v4 as uuid } from "uuid"
 export const Todos = ()=>{
     const [todos,setTodos] = useState([])
 
     const handellclick = (text)=>{
-        setTodos([...todos,text])
+        const payloade = {
+            title:text,
+            id:uuid(),
+            status:false
+        }
+        setTodos([...todos,payloade])
+    }
+
+    const DeleteTodo = (id)=>{
+       
     }
   return (
       <div>
           <Todosinput handellclick={handellclick}/>
           {todos.map((e)=>{
-              return <Todositem title = {e}/>
+              return <Todositem key={e.id} DeleteTodo={DeleteTodo} title = {e.title} status ={e.status}/>
           })}
       </div>
   )
